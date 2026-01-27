17d7335b21d6b7fd1a71fca00ba797fa6597c988
azetage <guillermoavilazamora@gmail.com>
2026-01-27 11:50:17 -0300

    hola mundo

--- controllers/recepcionController.js
+++ controllers/recepcionController.js
@@ -13,6 +13,7 @@ const subirDebitos = [
   upload.single('archivoExcel'),
   (req, res) => {
     try {
+      const nombreOriginal = req.file.originalname;
       const workbook = XLSX.readFile(req.file.path);
       const sheetName = workbook.SheetNames[0];
       const datos = XLSX.utils.sheet_to_json(
@@ -32,6 +33,20 @@ const subirDebitos = [
                     '__EMPTY_24', '__EMPTY_25', '__EMPTY_26',
                     '__EMPTY_27', '__EMPTY_28', '__EMPTY_29']
       const Diputados= [ 'NRO_AGENTE','APEYNOM', 'CUIL','MONTO','COD','PAGO','TIPO','DNI_DESC']
+
+      const Educacion= [
+                    '__EMPTY',    '__EMPTY_1',
+                    '__EMPTY_2',  '__EMPTY_3',
+                    '__EMPTY_4',  '__EMPTY_5',
+                    '__EMPTY_6',  '__EMPTY_7',
+                    '__EMPTY_8',  '__EMPTY_9',
+                    '__EMPTY_10', '__EMPTY_11',
+                    '__EMPTY_12', '__EMPTY_13',
+                    '__EMPTY_14', '__EMPTY_15',
+                    '__EMPTY_16', '__EMPTY_17',
+                    '__EMPTY_18', '__EMPTY_19'
+                    ]
+      const PoderJudicial = [ 'CUIL', 'NRO_AGENTE', 'APEYNOM', 'MONTO','COD','PAGO','TIPO','DNI_DESC']
 
       let data //datos mapa formateados
       let Organismo= ""
@@ -73,13 +88,65 @@ const subirDebitos = [
                 })
                          // console.log(data)
               }
-      else{ 
+      else if (JSON.stringify(encabezados) == JSON.stringify(Diputados)) {
+ 
           console.log("diputados")
           console.log(encabezados)
+
+          Organismo= "DIPUTADOS "
+          data =   datos.map(item   => { 
+          return {
+                      PERIODO:    nombreOriginal,
+                      NRO_AGENTE: item.NRO_AGENTE,
+                      DNI_DESC:   item.DNI_DESC,
+                      APEYNOM:    item.APEYNOM,                                
+                      MONTO:      item.MONTO,                            
+                 
+                  }
+                })
+          
+         }
+          else if (JSON.stringify(encabezados) == JSON.stringify(Educacion)) {
+ 
+          console.log("Educacion")
+          console.log(encabezados)
+          console.log(datos[0],datos[1],datos[2],datos[3],datos[4])
+
+          Organismo= "EDUCACION "
+          const periodo = datos[2]['__EMPTY_7']
+
+          data =   datos.slice(4).map(item   => { 
+          return {
+                      PERIODO:    periodo,
+                      NRO_AGENTE: item.__EMPTY,
+                      DNI_DESC:   item.__EMPTY,
+                      APEYNOM:    item.__EMPTY_2,                                
+                      MONTO:      item.__EMPTY_17,                            
+                 
+                  }
+                })
+          
+         }
+         else if (JSON.stringify(encabezados) == JSON.stringify(PoderJudicial)) {
+ 
+          console.log("PoderJudicial")
+          console.log(encabezados)
+
+          Organismo= "PODER JUDICIAL "
+          data =   datos.map(item   => { 
+          return {
+                      PERIODO:    nombreOriginal,
+                      NRO_AGENTE: item.NRO_AGENTE,
+                      DNI_DESC:   String(item.CUIL).substring(2, 10),
+                      APEYNOM:    item.APEYNOM,                                
+                      MONTO:      item.MONTO,                            
+                 
+                  }
+                })
           
          }
   
-      res.render('main/resultadoTabla', { data, pagina : Organismo});
+      res.render('main/resultadoTabla', { data, pagina : Organismo, archivo : nombreOriginal});
 
     } catch (error) {
       console.error(error);
